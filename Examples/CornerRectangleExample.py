ad8b40cc4a96b81add047b1ee5e35bc401b38633
Lepsy Goyal <62747435+lepsygoyal@users.noreply.github.com>
2021-05-19 00:54:18 +0530

    Add files via upload

---
+++ Examples/CornerRectangleExample.py
@@ -0,0 +1,20 @@
+import cvzone
+import cv2
+
+cap = cv2.VideoCapture(0)
+detector = cvzone.HandDetector()
+
+while True:
+    # Get image frame
+    success, img = cap.read()
+
+    # Find the hand and its landmarks
+    img = detector.findHands(img, draw=False)
+    lmList, bbox = detector.findPosition(img, draw=False)
+    if bbox:
+        # Draw  Corner Rectangle
+        cvzone.cornerRect(img, bbox)
+
+    # Display
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
